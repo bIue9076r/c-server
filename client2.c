fca30cd29fe724ed34e9419420fc25a58cc65fd4
blue <nobfi517@gmail.com>
2024-11-03 04:52:38 -0600

    add port

--- client2.c
+++ client2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+// Client code
+#define PORT 1234
 
 int main(void){
 	return 0;
--- server2.c
+++ server2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+// Server Code
+#define PORT 1234
 
 int main(void){
 	return 0;
