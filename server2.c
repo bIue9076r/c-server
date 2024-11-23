2821ea03539a1e34e5e74da6fea116ce50e435ab
blue <nobfi517@gmail.com>
2024-11-23 03:14:17 -0600

    server2 change

--- server2.c
+++ server2.c
@@ -13,5 +13,10 @@ char request[REQUEST_LEN];
 char* response;
 
 int main(void){
+	struct sockaddr_in addr;
+	int addr_len = sizeof(addr);
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(PORT);
 	return 0;
 }
